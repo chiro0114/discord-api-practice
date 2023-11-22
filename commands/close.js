732c226229c6f678943201af023a0ba0026a9f2e
chiro0114 <vqrichiro@gmail.com>
2023-11-22 19:11:55 +0900

    feat: データの最終完成

--- commands/close.js
+++ commands/close.js
@@ -1,23 +1,37 @@
-import { SlashCommandBuilder } from "discord.js";
-import { client } from "../index.js";
+import { SlashCommandBuilder } from 'discord.js';
+import { client } from '../index.js';
 
 export default {
   data: new SlashCommandBuilder()
-    .setName("close")
-    .setDescription("質問完了後、このコマンドを流し投稿をクローズする"),
-  async execute({ channelId }) {
-    client.channels.fetch(channelId).then((channel) => {
-      channel.messages.fetch().then((a) => {
-        const contentArray = a.map((e) => ({
-          content: e.content,
-          userName: e.author.globalName
-            ? e.author.globalName
-            : e.author.username,
+    .setName('close')
+    .setDescription('質問完了後、このコマンドを流し投稿をクローズする'),
+  async execute(interaction) {
+    const channel = await client.channels.fetch(interaction.channelId);
+    const messages = await channel.messages.fetch();
+    const faqItems = messages
+      .filter((message) => !message.author.bot)
+      .map((message) => {
+        const attachments = message.attachments.map((attachment) => ({
+          id: attachment.id,
+          url: attachment.url,
+          contentType: attachment.contentType,
         }));
-        channel.setName(`【解決済】${channel.name}`).then(() => {
-          channel.setArchived(true);
-        });
+        return {
+          content: message.content,
+          userName: message.author.globalName
+            ? message.author.globalName
+            : message.author.username,
+          attachments,
+        };
       });
+    const faqData = {
+      id: channel.id,
+      title: channel.name,
+      contents: [...faqItems.reverse()],
+    };
+    channel.setName(`【解決済】${channel.name}`).then(() => {
+      channel.setArchived(true);
     });
+    await interaction.reply('このチャットは終了しました');
   },
 };
